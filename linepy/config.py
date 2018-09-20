3bdde314d710b3e5064166989aff544f516250e3
Dhenza15 <38450225+dhenza1415@users.noreply.github.com>
2018-09-20 03:58:13 +0000

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -27,10 +27,10 @@ class Config(object):
         'LINE_SERVICES': '1459630796'
     }
 
-    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[96]
-    APP_VER     = '7.18.1'
+    APP_TYPE    = ApplicationType._VALUES_TO_NAMES[368]
+    APP_VER     = '2.1.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'SLACKBOT'
+    SYSTEM_NAME = 'SILENTBOT'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
